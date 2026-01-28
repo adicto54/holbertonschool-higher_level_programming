a01cebd47cad955ab7f4ce4bd2e868cbc20b24b2
Your Name <you@example.com>
2026-01-28 19:36:44 +0000

    dsd

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,7 @@
 --dasdsda
+USE hbtn_0d_usa;
+
 SELECT *
 FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+WHERE state_id = (SELECT id FROM states WHERE name = 'California' LIMIT 1)
 ORDER BY id ASC;
